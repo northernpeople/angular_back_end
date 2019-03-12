d29109b4c754a24e874a5ed55207cb65fe68b04d
Stepan Razdobarov <northernpeople@gmail.com>
2019-03-11 21:21:17 -0600

    removed old files

--- src/main/java/com/stepan/task_manager/user/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.stepan.task_manager.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, String> {
-
-	User findByEmail(String email);
-
-}
